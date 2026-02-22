4fdf6880925157dc2b0b399df0dd2cc8ccefd1e2
Jules Chamoy <jules.chamoy@universite-paris-saclay.fr>
2026-02-22 22:00:07 +0100

    vstar

--- SRC/Liquide.cpp
+++ SRC/Liquide.cpp
@@ -41,14 +41,13 @@ void Liquide::SolveurPression(double eps, double dt, double omega, int Maxiter){
     int ny = this->grid.NY();
     Champ rhs(nx,ny);
 
-<<<<<<< HEAD:Liquide.cpp
-    for(int x=1; x<nx-1; x++){
-        for(int y=1; y<ny-1; y++){
-            rhs(x,y)= (rho_l/dt)*(*this).div_u_star(x,y);
+    for(int x = 0; x < nx; x++){
+        for(int y = 0; y < ny; y++){
+            rhs(x,y) = (rho_l/dt)*(*this).div_u_star(x,y);
         }
     }
 
-    PoissonSOR((*this).P(),rhs, this->grid, omega, Maxiter, eps);
+    PoissonSOR(this->p,rhs, this->grid, omega, Maxiter, eps);
 }
 
 void Liquide::calc_ux_star(int x, int y, double dt){
@@ -77,16 +76,3 @@ void Liquide::calc_tot_U_star(double dt){
         }
     }
 }
-
-//void Liquide::Contrib(double dt){
-//}
-=======
-    for(int x = 0; x < nx; x++){
-        for(int y = 0; y < ny; y++){
-            rhs(x,y) = (rho_l/dt)*(*this).div_u_star(x,y);
-        }
-    }
-
-    PoissonSOR(this->p,rhs, this->grid, omega, Maxiter, eps);
-}
->>>>>>> 38058892cc183d2e4c67267c7ac3e05c28da7abc:SRC/Liquide.cpp
